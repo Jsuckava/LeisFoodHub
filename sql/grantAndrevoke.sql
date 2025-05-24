57bd6a1415ae5f9b3bb5bb7afbb3fd777ec8490f
Jsuckava <147977927+Jsuckava@users.noreply.github.com>
2025-05-24 09:01:56 +0800

    Add files via upload

---
+++ sql/grantAndrevoke.sql
@@ -0,0 +1,23 @@
+USE lei_foodhubDb;
+GO
+--
+CREATE LOGIN FinalsNewAdmin WITH PASSWORD = 'LeiAllenHEHE';
+GO
+--
+CREATE USER FinalsNewAdminDb FOR LOGIN FinalsNewAdmin;
+GO
+--
+ALTER ROLE db_datareader ADD MEMBER FinalsNewAdminDb;
+ALTER ROLE db_datawriter ADD MEMBER FinalsNewAdminDb;
+GO
+--
+REVOKE SELECT ON dbo.LF_Users FROM FinalsNewAdminDb;
+REVOKE EXECUTE FROM FinalsNewAdminDb;
+GO
+--
+GRANT SELECT ON dbo.LF_Users TO FinalsNewAdminDb;
+GRANT EXECUTE TO FinalsNewAdminDb;
+GO
+--
+SELECT * FROM dbo.LF_Users;
+GO
